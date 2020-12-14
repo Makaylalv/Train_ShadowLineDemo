0bfb967868261db8f75f6bfebd4f60709486338d
YeZimeng <1060137398@qq.com>
2020-12-14 09:23:43 +0800

    yzm

--- app/src/main/java/com/example/train_shadowlinedemo/ConfigUtil.java
+++ app/src/main/java/com/example/train_shadowlinedemo/ConfigUtil.java
@@ -1,6 +1,6 @@
 package com.example.train_shadowlinedemo;
 
 public class ConfigUtil {
-    public static final String SERVER_ADDR="http://192.168.43.180:8080/ShadowLine/";
+    public static final String SERVER_ADDR="http://192.168.56.1:8080/ShadowLine/";
     //192.168.43.27
 }
