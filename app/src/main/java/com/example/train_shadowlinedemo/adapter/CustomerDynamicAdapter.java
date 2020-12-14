1ed82d84ce8ce1c09d578a00500354a9c15b6808
xuyuanzhi051 <191497805@qq.com>
2020-12-14 10:10:32 +0800

    界面fragment的改动

--- app/src/main/java/com/example/train_shadowlinedemo/adapter/CustomerDynamicAdapter.java
+++ app/src/main/java/com/example/train_shadowlinedemo/adapter/CustomerDynamicAdapter.java
@@ -537,13 +537,16 @@ public class CustomerDynamicAdapter  extends BaseAdapter {
     private void showShare() {
         OnekeyShare oks = new OnekeyShare();
 // title标题，微信、QQ和QQ空间等平台使用
-        oks.setTitle("分享");
+        oks.setTitle("影行");
 // titleUrl QQ和QQ空间跳转链接
-        oks.setTitleUrl("http://sharesdk.cn");
+        oks.setTitleUrl("http://baidu.com");
 // text是分享文本，所有平台都需要这个字段
-        oks.setText("我是分享文本");
+        oks.setText(dynamics.get(3).getDynamicContent());
 // setImageUrl是网络图片的url
-        oks.setImageUrl("https://hmls.hfbank.com.cn/hfapp-api/9.png");
+       // oks.setImageUrl("https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3684536465,3063644397&fm=26&gp=0.jpg");
+        String []imgs={"https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3684536465,3063644397&fm=26&gp=0.jpg","https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=3684536465,3063644397&fm=26&gp=0.jpg"};
+        oks.setImageArray(imgs);
+
 // url在微信、Facebook等平台中使用
         oks.setUrl("http://sharesdk.cn");
 // 启动分享GUI
